579b8f0e6e66578d14a3d806879168661c54c146
Nick Knowlton <nknowlton@nskstat.com>
2019-05-29 11:30:03 +1200

    Added Singularity Build Scripts (#5)
    * Changed Samtools to conda
    
    * Created Singularity Build Script
    
    # Updated GitIgnore to include *.simg

---
+++ singularity/sing-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Build in Alphabetical Order
+echo "Building Adtex"
+singularity build --force adtex.simg docker://nknowlton/bioinformatics-containers:adtex
+echo "Building Bedtools"
+singularity build bedtools-0.3.simg docker://nknowlton/bioinformatics-containers:bedtools-0.3
+echo "Building BWA"
+singularity build --force bwa-0.7.17.simg docker://nknowlton/bioinformatics-containers:bwa-0.7.17
+echo "Building Cutadapt"
+singularity build --force cutadapt-2.3.simg docker://nknowlton/bioinformatics-containers:cutadapt-2.3
+echo "Building FastQC"
+singularity build --force fastqc-0.11.7.simg docker://nknowlton/bioinformatics-containers:fastqc-0.11.7
+echo "Building GATK4"
+singularity build --force gatk-4.1.2.0.simg docker://broadinstitute/gatk:4.1.2.0
+echo "Building Lumpy"
+singularity build --force lumpy-0.3.simg docker://nknowlton/bioinformatics-containers:lumpy-0.3
+echo "Building Picard"
+singularity build --force picard-2.18.4.simg docker://nknowlton/bioinformatics-containers:picard-2.18.4
+echo "Building Samtools"
+singularity build --force samtools-1.9.simg docker://nknowlton/bioinformatics-containers:samtools-1.9
+echo "Building VarScan"
+singularity build --force varscan-2.4.3.simg docker://nknowlton/bioinformatics-containers:varscan-2.4.3
